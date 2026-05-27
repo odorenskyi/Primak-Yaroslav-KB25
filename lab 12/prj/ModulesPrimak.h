63943cb0dc9199503a4f2c7f92f930807aadf95e
Yroslav1902 <primak1902@gmail.com>
2026-05-27 17:50:08 +0300

    Створив файл .h

---
+++ lab 12/prj/ModulesPrimak.h
@@ -0,0 +1,44 @@
+#ifndef MODULESPRIMAK_H
+#define MODULESPRIMAK_H
+
+#include <cmath>
+#include <stdexcept>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+// Клас Кулі
+class ClassLab12_Primak {
+private:
+    double radius; // інкапсуляція
+
+public:
+    ClassLab12_Pryzvysche(double r = 1.0); // Конструктор
+    void setRadius(double r);              // Сетер
+    double getRadius() const;              // Гетер
+    double calculateSurfaceArea() const;   // Метод обчислення площі
+};
+
+// Реалізація методів ПОЗА тілом класу з inline
+inline ClassLab12_Pryzvysche::ClassLab12_Pryzvysche(double r) {
+    setRadius(r);
+}
+
+inline void ClassLab12_Primak::setRadius(double r) {
+    if (r <= 0.0) {
+        throw std::invalid_argument("Радіус має бути більшим за 0!");
+    }
+    this->radius = r;
+}
+
+inline double ClassLab12_Primak::getRadius() const {
+    return this->radius;
+}
+
+inline double ClassLab12_Primak::calculateSurfaceArea() const {
+    return 4.0 * M_PI * std::pow(this->radius, 2);
+}
+
+#endif
+
